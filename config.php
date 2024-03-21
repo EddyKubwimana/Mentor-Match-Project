799a51803a04685251e5985af4b6859dce5e1f1f
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2024-03-21 20:31:44 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
   echo "I am inside the script";
-  $db_host = 'lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-  $db_user = 'c5klz0o20nee6ae9';
-  $db_password = 'l2fbd7spxdgwxg99';
-  $db_db = 'dgektj0gzl6h7k2x';
+  $servername = 'lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+  $username = 'c5klz0o20nee6ae9';
+  $password = 'l2fbd7spxdgwxg99';
+  $database = 'dgektj0gzl6h7k2x';
   $port = 3306;
 
 // Create connection
