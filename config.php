7f917a500d8e51772aff2fb3636ec75937b29fc7
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2024-03-21 20:29:37 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
+  echo "I am inside the script";
   $db_host = 'lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
   $db_user = 'c5klz0o20nee6ae9';
   $db_password = 'l2fbd7spxdgwxg99';
